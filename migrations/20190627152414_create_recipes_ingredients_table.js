24510680d82a94f4bbd558fb572b21f32346637f
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-27 15:42:54 -0600

    knexfile, ingredients table updated

--- migrations/20190627152414_create_recipes_ingredients_table.js
+++ migrations/20190627152414_create_recipes_ingredients_table.js
@@ -1,8 +1,10 @@
-
-exports.up = function(knex) {
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('ingredient', table => {
+        table.increments();
+        table.string('name', 128).notNullable().unique()
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('ingredient');
+  };
