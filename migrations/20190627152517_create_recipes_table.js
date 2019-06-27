7a96b502b7abe64fdee912401052c3a33bfb301b
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-27 15:43:32 -0600

    recipes table updated

--- migrations/20190627152517_create_recipes_table.js
+++ migrations/20190627152517_create_recipes_table.js
@@ -1,8 +1,17 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipes', table => {
+        table.increments();
+        table.string('name', 128).notNullable().unique();
+        table.integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE')
+    })
+  };
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('recipes');
+  };
