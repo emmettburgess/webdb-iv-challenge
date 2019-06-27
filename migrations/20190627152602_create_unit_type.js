c2e8c593a8b650da4568d55c675f600b3c0feef2
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-27 15:44:40 -0600

    unit type updated

--- migrations/20190627152602_create_unit_type.js
+++ migrations/20190627152602_create_unit_type.js
@@ -1,8 +1,10 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('unit_type', table => {
+        table.increments();
+        table.string('name', 128).notNullable().unique()
+    })
+  };
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('unit_type');
+  };
